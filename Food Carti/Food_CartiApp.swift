073bae9d2ff30800f95160c3ba932fa6b231df94
domchang1 <dominicchang08@gmail.com>
2024-11-26 15:31:47 -0800

    Initial Commit

---
+++ Food Carti/Food_CartiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_CartiApp.swift
+//  Food Carti
+//
+//  Created by Dominic Chang on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Food_CartiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
