a181907ee343a56ec8307296d9d6d341874bf74f
domchang1 <dominicchang08@gmail.com>
2024-11-29 12:53:58 -0800

    added add review functionality

--- Food Carti/View Models/AppViewModel.swift
+++ Food Carti/View Models/AppViewModel.swift
@@ -14,7 +14,10 @@ class AppViewModel: ObservableObject {
         Location(name: "Kim's Food Truck", coordinate: CLLocationCoordinate2D(latitude: 39.953916, longitude: -75.197531), description: "An amazing and convenient Chinese food truck!", image: "kims"),
         Location(name: "Tyson Bees Food Truck", coordinate: CLLocationCoordinate2D(latitude: 39.950096, longitude: -75.191895), description: "A delicious and creative Asian Mexican fusion food truck!", image: "tysonbees"),
         Location(name: "Penn Halal Gyro on Locust", coordinate: CLLocationCoordinate2D(latitude: 39.952930, longitude: -75.202779), description: "A cheap and yummy Halal food truck!", image: "pennhalallocust"),
-        Location(name: "Hemo's Food Truck", coordinate: CLLocationCoordinate2D(latitude: 39.950787, longitude: -75.195850), description: "A convenient and delicious breakfast/brunch food truck!", image: "hemos")
+        Location(name: "Hemo's Food Truck", coordinate: CLLocationCoordinate2D(latitude: 39.950787, longitude: -75.195850), description: "A convenient and delicious breakfast/brunch food truck!", image: "hemos"),
+        Location(name: "Tacos Don Memo", coordinate: CLLocationCoordinate2D(latitude: 39.95197702796157, longitude: -75.19926975474803), description: "A yummy Mexican food truck!", image: "tacosdonmemo"),
+        Location(name: "Bui's Lunch Truck", coordinate: CLLocationCoordinate2D(latitude: 39.951656866004065, longitude: -75.19920484599017), description: "A convenient and delicious breakfast/lunch food truck!", image: "buis"),
+        
     ]
 
     @Published var reviews: [Review] = [
--- Food Carti/Views/MapView.swift
+++ Food Carti/Views/MapView.swift
@@ -54,6 +54,7 @@ struct MapView: View {
             .sheet(isPresented: $showingPopUp) {
                 if let location = selectedLocation {
                     LocationPopupView(location: location)
+                        .environmentObject(viewModel)
                         .presentationDetents([.medium])
                         .background(Color.gray.opacity(0.1))
                         .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -64,7 +65,13 @@ struct MapView: View {
 }
 
 struct LocationPopupView: View {
+    @EnvironmentObject var viewModel: AppViewModel
     let location: Location
+    @State var addReview: Bool = false
+    @State var addedReview: Bool = false
+    @State var rating = 5.0
+    @State var isEditing = false
+    @State var descriptionString = ""
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
@@ -76,10 +83,68 @@ struct LocationPopupView: View {
                 
                 Text(location.description)
                     .font(.body)
-                Image(location.image)
-                    .resizable()
-                    .scaledToFit()
                     //.frame(width: 25, height: 25)
+                if !addReview {
+                    Image(location.image)
+                        .resizable()
+                        .scaledToFit()
+                    Button(action:{
+                        withAnimation {
+                            addReview.toggle()
+                        }
+                    }) {
+                        Text("Add Review")
+                            .padding()
+                            .fontWeight(.medium)
+                            .background(addedReview ? Color.gray : Color.blue)
+                            .foregroundColor(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    .disabled(addedReview)
+                } else {
+                    VStack {
+                        Slider(
+                            value: $rating,
+                            in: 0...10,
+                            step: 0.1
+                        ) {
+                            Text("Rating")
+                        } minimumValueLabel: {
+                            Text("0")
+                        } maximumValueLabel: {
+                            Text("10")
+                        } onEditingChanged: { editing in
+                            isEditing = editing
+                        }
+                        Text(String(format: "%.1f", rating))
+                                .foregroundColor(isEditing ? .indigo : .blue)
+                        TextField(
+                            "Enter Description",
+                            text: $descriptionString
+                        )
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        Button(action: { // fix when review class changed
+                            let review: Review = .init(
+                                locationName: location.name,
+                                rating: rating,
+                                description: descriptionString,
+                                reviewer: "Bob")
+                            viewModel.reviews.insert(review, at: 0)
+                            addReview = false
+                            addedReview = true
+                        }) {
+                            Text("Submit")
+                                .padding()
+                                .fontWeight(.medium)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                        
+                    }
+                    .transition(.scale)
+                }
             }
             .padding()
             .navigationBarItems(trailing: Button("Close") {
